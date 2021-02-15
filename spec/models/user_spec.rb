0c48a034ecfe4f24e032cff97074e28219a2e586
akarinaakarina <78073439+akarinaakarina@users.noreply.github.com>
2021-02-15 18:47:00 +0900

    ユーザー管理機能の実装（修正3）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -69,6 +69,11 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
+      it 'emailは@を含まないと登録できない' do
+        @user.email = 'aaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
       it 'passwordが5文字以下では登録できない' do
         @user.password = '00000'
         @user.password_confirmation = '00000'
