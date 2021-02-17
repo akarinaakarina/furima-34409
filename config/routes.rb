5edb9a98438597fbab3f12bda5903ed818897dbb
akarinaakarina <78073439+akarinaakarina@users.noreply.github.com>
2021-02-17 09:10:51 +0900

    商品出品機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-
+  resources :items do
+    resources :buys, only: :create
+  end
+  resources :users, only: :show
 end
