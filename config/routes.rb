cb9ca3cc0664a55adad790873fd14b9d65fb08f7
akarinaakarina <78073439+akarinaakarina@users.noreply.github.com>
2021-02-18 11:34:47 +0900

    商品削除機能の実装（修正）

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-    resources :buys, only: :create
+    # resources :buys, only: :create
   end
-  resources :users, only: :show
 end
