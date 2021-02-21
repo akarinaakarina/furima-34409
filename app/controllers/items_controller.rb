623cb586986b800acad77cca66fecaa2db6afe10
akarinaakarina <78073439+akarinaakarina@users.noreply.github.com>
2021-02-21 14:21:54 +0900

    商品購入機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :move_to_index2, only: :edit
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -52,5 +53,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless current_user.id == @item.user.id
+  end
+
+  def move_to_index2
+    redirect_to action: :index if @item.buy.present?
   end
 end
